ca35240255fe21618a0ef5a219372df4a35877d7
Michael Pratt <michael@pratt.im>
2015-12-27 18:25:33 -0800

    Fix my dumb logic in uitoa

--- util/string.cc
+++ util/string.cc
@@ -26,7 +26,7 @@ char *uitoa(uint32_t number, char *buf, uint32_t len, uint32_t base) {
     buf[i] = '\0';
 
     int end = i - 1;
-    for (int j = 0; j < end; j++) {
+    for (int j = 0; j <= end/2; j++) {
         char temp = buf[j];
         buf[j] = buf[end-j];
         buf[end-j] = temp;
