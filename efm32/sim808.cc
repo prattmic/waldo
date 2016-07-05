e692328353080c7cd38792ead1bd385909098cb2
Michael Pratt <michael@pratt.im>
2016-07-05 16:34:36 -0700

    SQUASH: this works pretty well

--- efm32/sim808.cc
+++ efm32/sim808.cc
@@ -47,4 +47,61 @@ EFMSIM808::EFMSIM808(std::unique_ptr<io::ByteIO> io)
     }
 }
 
+util::Status EFMSIM808::SetPower(bool on) {
+    // The manual says this can take up to 33s.
+    auto end = std::chrono::system_clock::now() + std::chrono::seconds(33);
+
+    LOG(INFO) << "Powering: " << on;
+
+    auto statusor = PowerStatus();
+    if (!statusor.ok())
+        return statusor.status();
+
+    // Already in desired state?
+    if (statusor.Value() == on)
+        return util::Status::OK;
+
+    // Hold PWRKEY high until PWRSTAT goes high.
+    auto status = GPIOOutput(GPIOPort::PortC, 0, true);
+    if (!status.ok())
+        return status;
+
+    while (statusor.ok() && statusor.Value() != on) {
+        if (std::chrono::system_clock::now() > end) {
+            GPIOOutput(GPIOPort::PortC, 0, false);
+            return util::Status(util::error::Code::DEADLINE_EXCEEDED,
+                                "SetPower timeout");
+        }
+
+        statusor = PowerStatus();
+    }
+
+    // PWRKEY back to low.
+    status = GPIOOutput(GPIOPort::PortC, 0, false);
+    if (!status.ok())
+        return status;
+
+    // Power is now either in the desired state or there was an error.
+    return statusor.status();
+}
+
+util::Status EFMSIM808::PowerCycle() {
+    auto statusor = PowerStatus();
+    if (!statusor.ok())
+        return statusor.status();
+
+    // Power off if not already off.
+    if (statusor.Value()) {
+        auto status = SetPower(false);
+        if (!status.ok())
+            return status;
+    }
+
+    auto now = std::chrono::system_clock::now();
+    auto end = now + std::chrono::seconds(1);
+    while (std::chrono::system_clock::now() < end);
+
+    return SetPower(true);
+}
+
 }  // namespace efm32
--- efm32/sim808.h
+++ efm32/sim808.h
@@ -2,6 +2,7 @@
 #define EFM32_SIM808_H_
 
 #include <chrono>
+#include <chrono>
 #include <memory>
 #include "efm32/gpio.h"
 #include "external/com_github_prattmic_nanopb/util/task/status.h"
@@ -21,55 +22,10 @@ class EFMSIM808 : public sim808::SIM808 {
     }
 
     // Turn power on or off.
-    // TODO(prattmic): timeout
-    util::Status SetPower(bool on) {
-        // 5s timeout.
-        auto end = std::chrono::system_clock::now() + std::chrono::seconds(5);
+    util::Status SetPower(bool on);
 
-        auto statusor = PowerStatus();
-        if (!statusor.ok())
-            return statusor.status();
-
-        // Already in desired state?
-        if (statusor.Value() == on)
-            return util::Status::OK;
-
-        // Hold PWRKEY high until PWRSTAT goes high.
-        auto status = GPIOOutput(GPIOPort::PortC, 0, true);
-        if (!status.ok())
-            return status;
-
-        while (statusor.ok() && statusor.Value() != on) {
-            if (std::chrono::system_clock::now() > end)
-                return util::Status(util::error::Code::DEADLINE_EXCEEDED,
-                                    "SetPower timeout");
-
-            statusor = PowerStatus();
-        }
-
-        // PWRKEY back to low.
-        status = GPIOOutput(GPIOPort::PortC, 0, false);
-        if (!status.ok())
-            return status;
-
-        // Power is now either in the desired state or there was an error.
-        return statusor.status();
-    }
-
-    util::Status PowerCycle() {
-        auto statusor = PowerStatus();
-        if (!statusor.ok())
-            return statusor.status();
-
-        // Power off if not already off.
-        if (statusor.Value()) {
-            auto status = SetPower(false);
-            if (!status.ok())
-                return status;
-        }
-
-        return SetPower(true);
-    }
+    // Turn power off then on.
+    util::Status PowerCycle();
 };
 
 }  // namespace efm32
